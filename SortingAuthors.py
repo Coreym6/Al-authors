25c197664891f5f3edc6be27096ba3fe62db3190
Coreym6 <cmcdani3@samford.edu>
2023-11-08 11:45:02 -0600

    Added sort function

--- SortingAuthors.py
+++ SortingAuthors.py
@@ -4,3 +4,12 @@ def sort_csv(csv_file,sort_column, Columns_to_print):
     csv_file = '150 Author Geodata.csv'
     sort_column = 'Lastname'
     Columns_to_print = ['Lastname','Firstname','Birthplace','Other Cities','County','education','adult residence','place of burial']
+
+    with open(csv_file,'r') as csvfile:
+        csv_data = list(csv.DictReader(csvfile))
+        sorted_data = sorted(csv_data, key=lambda row: row[sort_column])
+        for row in sorted_data:         #for loop location that gives the output 
+            output_row = {column: row[column] for column in Columns_to_print}
+            print(output_row)
+# call back to the function right here 
+    sort_csv(csv_file, sort_column, Columns_to_print)
