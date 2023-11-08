5603d7e5de19798d082ce37a207e1fcc52663778
Coreym6 <cmcdani3@samford.edu>
2023-11-08 10:42:16 -0600

    added a few variables

--- SortingAuthors.py
+++ SortingAuthors.py
@@ -1,4 +1,6 @@
 import csv
 
-def sort_csv():
-    csv_file = ''
+def sort_csv(csv_file,sort_column, Columns_to_print):
+    csv_file = '150 Author Geodata.csv'
+    sort_column = 'Lastname'
+    Columns_to_print = ['Lastname','Firstname','Birthplace','Other Cities','County','education','adult residence','place of burial']
