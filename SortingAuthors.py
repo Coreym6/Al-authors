34c2cc403c180bf9d9c19cc25a8a29939d170c60
Coreym6 <cmcdani3@samford.edu>
2023-11-08 12:26:36 -0600

    reordered again ;(

--- SortingAuthors.py
+++ SortingAuthors.py
@@ -1,9 +1,6 @@
 import csv
 
 def sort_csv(csv_file,sort_column, Columns_to_print):
-    csv_file = '150 Author Geodata.csv'
-    sort_column = 'Lastname'
-    Columns_to_print = ['Lastname','Firstname','Birthplace','Other Cities','County','education','adult residence','place of burial', 'Lifespan', 'Colors']
 
     with open(csv_file,'r') as csvfile:
         csv_data = list(csv.DictReader(csvfile))
@@ -12,7 +9,9 @@ def sort_csv(csv_file,sort_column, Columns_to_print):
             output_row = {column: row[column] for column in Columns_to_print}
             print(output_row)
 # call back to the function right here 
-
+csv_file = '150 Author Geodata.csv'
+sort_column = 'Lastname'
+Columns_to_print = ['Lastname','Firstname','Birthplace','Other Cities','County','education','adult residence','place of burial', 'Lifespan', 'Colors']
 
 #sort_csv(csv_file, sort_column, Columns_to_print)
    
