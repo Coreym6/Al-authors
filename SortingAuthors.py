621e43f19ab27edef02fe2a26977a76dbd00856b
Coreym6 <cmcdani3@samford.edu>
2023-11-08 11:59:26 -0600

    added life and colors

--- SortingAuthors.py
+++ SortingAuthors.py
@@ -3,7 +3,7 @@ import csv
 def sort_csv(csv_file,sort_column, Columns_to_print):
     csv_file = '150 Author Geodata.csv'
     sort_column = 'Lastname'
-    Columns_to_print = ['Lastname','Firstname','Birthplace','Other Cities','County','education','adult residence','place of burial']
+    Columns_to_print = ['Lastname','Firstname','Birthplace','Other Cities','County','education','adult residence','place of burial', 'Lifespan', 'Colors']
 
     with open(csv_file,'r') as csvfile:
         csv_data = list(csv.DictReader(csvfile))
@@ -13,4 +13,4 @@ def sort_csv(csv_file,sort_column, Columns_to_print):
             print(output_row)
 # call back to the function right here 
     sort_csv(csv_file, sort_column, Columns_to_print)
-    print(sort_csv)
+   
