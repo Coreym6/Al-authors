2a1268373a39c1ace69a6637b4a1ae37fef68f2e
Coreym6 <cmcdani3@samford.edu>
2024-01-26 10:16:34 -0600

    added a few changes

--- Sorting.py
+++ Sorting.py
@@ -13,9 +13,8 @@ def sort_csv(file_path, column, ascending=True):
 file_path = 'ALL Author Geography Metadata.csv'
 
 # Prompt the user for the column to sort by
-column = input("Enter the column to sort by: ")
+column = 'County'
 # want it to specifically be the county column
-
 
 # Prompt the user for the sorting order
 order = input("Enter the sorting order (asc/desc): ")
@@ -23,6 +22,8 @@ order = input("Enter the sorting order (asc/desc): ")
 # Convert the sorting order to a boolean value
 ascending = True if order.lower() == 'asc' else False
 
+#A place to put the First Name and Last Name column to return such, for ex. HENRY AARON 
+column_name = 'Author_First_Name_Last_Name'
 # making the list of the couunty associations 
 # all 67 counties in Alabama 
 county_list = [ [
@@ -94,12 +95,14 @@ county_list = [ [
         'Wilcox',
         'Winston'
     ]
-]
+] # REMEMBER THIS IS COLUMN 7 IN THE CSV FILE , "COUNTY"
 # this will be helpful for the 400+ authors on the list, but for the other amount, we will likely need something to sort out the html text
 # Sort the CSV file
-sorted_data = sort_csv(file_path, column, ascending)
+#COLUMN 3 is the column for 'Author_First_Name_Last_Name'
+sorted_data = sort_csv(file_path, column,column_name, ascending)
 file_path = 'ALL Author Geography Metadata.csv'
 column = 'County'
+column_name = 'Author_First_Name_Last_Name'
 order = 'asc'
 # Print the sorted data
 for row in sorted_data:
