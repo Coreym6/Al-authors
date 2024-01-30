fd71bc0e205caf58eb91efe2fea5dc69f171dcca
Coreym6 <cmcdani3@samford.edu>
2024-01-30 09:46:55 -0600

    column name

--- Sorting.py
+++ Sorting.py
@@ -19,6 +19,7 @@ descending = False if order.lower() == 'desc' else True
 
 #A place to put the First Name and Last Name column to return such, for ex. HENRY AARON 
 column_name = 'Author_First_Name_Last_Name'
+print(column_name)
 # making the list of the couunty associations 
 # all 67 counties in Alabama 
 county_list = [ [
