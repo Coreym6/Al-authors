9d21e3a2dc6ee13dd3a7977dc391945f8ea03f3e
Coreym6 <cmcdani3@samford.edu>
2024-01-30 21:24:57 -0600

    skip initial space to solve key error?

--- Sorting.py
+++ Sorting.py
@@ -105,6 +105,8 @@ def get_authors_by_county(file_path, encoding, county):
     with open(file_path, 'r', encoding=encoding) as file:
         reader = csv.DictReader(file)
         authors = [column.get('Author_First_Name_Last_Name', '') for column in reader if column.get('County') == county]
+        # will likely have to use the Dialect.skipinitialspace
+        #documentation https://docs.python.org/3/library/csv.html#csv.Dialect.skipinitialspace
     return authors
 
 
