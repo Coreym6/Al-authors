a0297aa798ea2eafec8d940659e0fddebb0eece7
Coreym6 <cmcdani3@samford.edu>
2024-02-20 16:44:37 -0600

    added list

--- Sorting.py
+++ Sorting.py
@@ -331,8 +331,10 @@ author_name = input("Enter the author you would like to search for: ")
 author_name = author_name.strip()
 def get_counties_by_author(csvData, author_name):
     csvData = pd.read_csv('/Users/coreymcdaniels/Desktop/Al Authors Local /Al-authors/Spring Semester dataset 2.csv')
+    #unnamed columns list comprehensions 
+    unnamed_columns = ['Unnamed: 6', 'Unnamed: 7', 'Unnamed: 10']
     filtered_csvData = csvData[csvData['Unnamed: 2'] == author_name]
-    counties = filtered_csvData['Unnamed: 6'].tolist()
+    counties = filtered_csvData[unnamed_columns].tolist()
     if len(counties) == 0:
         print("The author", author_name, "is not associated with any county.")
     else:
